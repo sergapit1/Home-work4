b446121dbe47cf2695be3bce1332c8b0aaa0bc6a
sergapit1 <serg.apit0@gmail.com>
2020-04-15 03:01:51 +0300

    Ready

--- src/by/academy/homework4/DataContainer.java
+++ src/by/academy/homework4/DataContainer.java
@@ -1,6 +1,7 @@
 package by.academy.homework4;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class DataContainer<E> {
     private E[] data;
@@ -25,4 +26,40 @@ public class DataContainer<E> {
     public E[] getData() {
         return data;
     }
+
+    static <E> void sort(DataContainer<E> item, Comparator<E> comparator) {
+        E temp;
+        boolean sorted = false;
+        while (!sorted) {
+            sorted = true;
+            for (int i = 0; i < item.getData().length - 1; i++) {
+                if (comparator.compare(item.getData()[i], item.getData()[i + 1]) == 0) {
+                    sorted = false;
+
+                   temp= item.getData()[i];
+                    item.getData()[i] = item.getData()[i + 1];
+                   item.getData()[i + 1] = temp;
+
+
+                }
+            }
+        }
+    }
+
+    public void delete(int index) {
+        for (int i = index; i < data.length - 1; i++)
+            data[i] = data[i + 1];
+        data = Arrays.copyOf(data, data.length - 1);
+    }
+
+    public void delete(E item) {
+        int hc = -1;
+        for (int i = 0; i < data.length - 1; i++)
+            if (data[i].hashCode() == item.hashCode()) {
+                hc = i;
+            } else if (hc != -1) delete(hc);
+    }
 }
+
+
+
--- src/by/academy/homework4/Main.java
+++ src/by/academy/homework4/Main.java
@@ -1,29 +1,49 @@
 package by.academy.homework4;
 
 import java.time.LocalDate;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
 
         RandomAgeName nr = new RandomAgeName();
         DataContainer<Animal> animalDataContainer = new DataContainer<>(new Animal[0]);
-
-        for (int i = 0; i < 100000; i++) {
+        for (int i = 0; i < 3; i++) {
             String nick = nr.getName();
             int age = nr.getAge();
             animalDataContainer.add(new Animal(nick, age));
-         //   System.out.println(animalDataContainer);
+            System.out.println(Arrays.toString(animalDataContainer.getData()));
+
         }
 
         DataContainer<Person> personDataContainer = new DataContainer<>(new Person[0]);
-        for (int i = 0; i < 100000; i++) {
+        for (int i = 0; i < 3; i++) {
             String nick = nr.getName();
             String password = nr.getPass();
             LocalDate date = nr.getDate();
             personDataContainer.add(new Person(nick, password, date));
-           // System.out.println(personDataContainer);
+            System.out.println(Arrays.toString(personDataContainer.getData()));
         }
 
+        DataContainer.sort(animalDataContainer, new AnimalAgeComparator());
+        System.out.println("Sort: ");
+        System.out.println(Arrays.toString(animalDataContainer.getData()));
 
+
+        animalDataContainer.delete(0);
+        System.out.println("Delete: ");
+        System.out.println(Arrays.toString(animalDataContainer.getData()));
+
+
+        DataContainer.sort(personDataContainer, new PersonRegistrationComparator());
+        System.out.println("Sort: ");
+        System.out.println(Arrays.toString(personDataContainer.getData()));
+
+
+        personDataContainer.delete(0);
+        System.out.println("Delete: ");
+        System.out.println(Arrays.toString(personDataContainer.getData()));
     }
+
 }
+
